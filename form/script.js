f761006af0567e767a4477aee3a347e307349f31
Júlia :) <148802056+vesky69@users.noreply.github.com>
2026-02-24 14:07:55 +0000

    script

--- form/script.js
+++ form/script.js
@@ -1,15 +1,41 @@
-Element.onclick = function IMC() {
-    let height, weight, IMC;
+/**
+ * @author Júlia Dias <jdoc173@gmail.com>
+ * @since 2026-02-24
+ * @version 1.0.0
+ * @description Esta função busca o campo data pelo id e aplica o atributo max com o dia atual.
+ */
 
-    IMC = weight / (height * height)
+function configDateLimit() {
+    const limit = new Date().toISOString().split("T")[0]
+    alert(limit)
+    const inputDate = document.getElementById("date");
 
-    if (IMC >= 30) {
-        console.log(`${IMC} = Obesidade`)
-    } else if (IMC >= 25) {
-        console.log(`${IMC} = Excesso de peso`)
-    } else if (IMC >= 18.5) {
-        console.log(`${IMC} = Peso normal`)
-    } else {
-        console.log(`${IMC} = Abaixo do peso`)
+    if(inputDate) {
+        inputDate.setAttribute("max", limit);
     }
+
+}
+document.addEventListener("DOMContentLoaded", configDateLimit);
+
+function IMC(event) {
+    event.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const weight = parseFloat(document.getElementById("weight").value);
+    const height = parseFloat(document.getElementById("height").value);
+
+    const IMC = weight / (height * height);
+
+    let classification = "";
+    if (IMC >= 30) {
+        classification = "Obesidade";
+    } else if (IMC >= 25) {
+        classification = "Excesso de peso";
+    } else if (IMC >= 18.5) {
+        classification = "Peso normal";
+    } else {
+        classification = "Abaixo do peso";
+    }
+
+    document.getElementById("resultIMC").innerHTML = `${name} seu IMC é ${IMC.toFixed(2)} (${classification})`;
 }
